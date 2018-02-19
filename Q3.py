4e51fcde219e08691ec2234df61a8aa6e9d1aef7
Seema Tamang <seema.tamang640@gmail.com>
2018-02-19 12:12:48 +0545

    Display the details of an employee generating the email.

---
+++ Q3.py
@@ -0,0 +1,20 @@
+class Employee:
+    def __init__(self,first_name,last_name,salary):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.salary=salary
+
+    def gen_email(self):
+        self.email=self.first_name + '.' + self.last_name + '@deerwalk.edu.np'
+
+    def display(self):
+         print("First name={}".format(self.first_name))
+         print("Last name={}".format(self.last_name))
+         print("Salary={}".format(self.salary))
+
+emp=Employee("Seema","Tamang",100000)
+emp.gen_email()
+emp.display()
+
+
+
